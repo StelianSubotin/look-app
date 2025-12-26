a4daa98c463a5d3b6eeef5b94b064fe4f35d3630
Stel <stel@example.com>
2025-12-26 22:45:57 +0200

    Add Tremor to Tailwind content - fix missing styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,8 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    // Tremor module
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
